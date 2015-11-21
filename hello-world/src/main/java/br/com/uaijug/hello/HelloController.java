af5bbbc8db89538f5e6fccaab0c9513b840eb752
josenaldo <josenaldo@gmail.com>
2015-11-21 01:53:56 -0200

    Criação da aplicação HelloWorld. Essa aplicação demonstra como é criado o um RestController básico.

---
+++ hello-world/src/main/java/br/com/uaijug/hello/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.uaijug.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(name="/hello")
+	public String getMessage() {
+		return "HelloWorldMessage";
+	}
+}
