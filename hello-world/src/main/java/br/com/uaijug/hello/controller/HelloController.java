24e1ea7005090ebaf8273a3bb691de6056440ece
josenaldo <josenaldo@gmail.com>
2015-11-21 02:04:25 -0200

    HelloController movido para o pacote de controllers

--- hello-world/src/main/java/br/com/uaijug/hello/controller/HelloController.java
+++ hello-world/src/main/java/br/com/uaijug/hello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package br.com.uaijug.hello;
+package br.com.uaijug.hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
